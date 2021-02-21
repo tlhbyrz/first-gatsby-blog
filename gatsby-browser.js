2d82850ea6eca0ea34eca3313efcfc3b73e2dc47
tlhbyrz <talhaboyraz13@gmail.com>
2021-02-21 16:51:59 +0300

    global styles eklendi

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,11 +1,27 @@
 import React from "react";
-import { ThemeProvider } from "styled-components"
+import { createGlobalStyle, ThemeProvider } from "styled-components"
 import Theme from "./src/themes/theme";
+
+
+const GlobalStyles = createGlobalStyle`
+    *{
+        box-sizing: border-box;
+        padding: 0;
+        margin: 0;
+    }
+
+    body,html{
+        font-family: ${props => props.theme.fonts.main};
+        background-color: ${props => props.theme.colors.light1};
+        height: 100%;
+    }
+`
 
 
 export const wrapRootElement = ({element}) => {
     return (
         <ThemeProvider theme={Theme}>
+            <GlobalStyles />
             {element}
         </ThemeProvider>
     )
