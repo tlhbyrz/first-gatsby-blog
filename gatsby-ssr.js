a86480c1bd8dc4bb78c9a2b55adfd7fce3186e7a
tlhbyrz <talhaboyraz13@gmail.com>
2021-02-21 15:01:49 +0300

    theme provider eklendi

---
+++ gatsby-ssr.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { ThemeProvider } from "styled-components"
+import Theme from "./src/themes/theme";
+
+
+export const wrapRootElement = ({element}) => {
+    return (
+        <ThemeProvider theme={Theme}>
+            {element}
+        </ThemeProvider>
+    )
+}
